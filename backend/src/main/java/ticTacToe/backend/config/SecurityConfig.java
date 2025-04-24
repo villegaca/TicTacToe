be9f81c4cbbe7a54e235d2d17a9848f88079a0f0
cvillega <cvillega@uno.edu>
2025-04-24 12:20:51 -0500

    added authentican manager bean to security config

--- backend/src/main/java/ticTacToe/backend/config/SecurityConfig.java
+++ backend/src/main/java/ticTacToe/backend/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package ticTacToe.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +25,11 @@ public class SecurityConfig {
                     .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManger(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 
 }
