1cb1539ea003a7a15e1e68ccfc3606529c99e68e
cvillega <cvillega@uno.edu>
2025-05-05 20:03:13 -0500

    debugged

--- backend/src/main/java/ticTacToe/backend/BackendApplication.java
+++ backend/src/main/java/ticTacToe/backend/BackendApplication.java
@@ -3,10 +3,17 @@ package ticTacToe.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("backend").filename(".env").load();
+		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+		System.setProperty("DATABASE_NAME", dotenv.get("DATABASE_NAME"));
+		System.setProperty("SECURITY_USERNAME", dotenv.get("SECURITY_USERNAME"));
+		System.setProperty("SECURITY_PASSWORD", dotenv.get("SECURITY_PASSWORD"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
